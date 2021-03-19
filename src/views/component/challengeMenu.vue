58f44a9018715df2817f9bdb581b1e0964fb6049
couy <couy69@gmail.com>
2021-03-19 11:15:14 +0800

    加入挑战菜单，被数值设计难住了

--- src/views/component/challengeMenu.vue
+++ src/views/component/challengeMenu.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="challengeMenu">
     <div class="menu">
-      <div class="Backpack">
+      <div class="Backpack" @click="openMenu('boss')">
         <img src="../../assets/icons/menu/1.png" alt="" />
         <span>boss挑战</span>
       </div>
-      <div class="Backpack">
+      <div class="Backpack" @click="openMenu('relic')">
         <img src="../../assets/icons/menu/1.png" alt="" />
         <span>宝物挑战</span>
       </div>
-      <div class="Backpack">
+      <div class="Backpack" @click="openMenu('equi')">
         <img src="../../assets/icons/menu/1.png" alt="" />
         <span>装备挑战</span>
       </div>
-      <div class="Backpack">
+      <div class="Backpack" @click="openMenu('loop')">
         <img src="../../assets/icons/menu/1.png" alt="" />
         <span>循环挑战</span>
       </div>
     </div>
-    <div class="dialog">
-      <div class="title">循环挑战</div>
+    <div class="dialog" v-if="challengeMenuDialog">
+      <div class="title">{{currentMenu.name}}</div>
       <div class="dmenu">
-        <div class="item" v-for="v in loop" :key="v.id">
+        <div class="item" v-for="v in currentMenu.menuList" :key="v.id" @click="resetCurrentMonster(v)">
           <div class="img">
             <div class="cover"></div>
             <img
@@ -42,39 +42,95 @@ export default {
   components: {},
   data() {
     return {
-      loop: [
-        {
-          type: "loop",
-          name: "lv1",
-          tachie: "lv1.gif"
-        },
-        {
-          type: "loop",
-          name: "lv2",
-          tachie: "lv2.gif"
-        },
-        {
-          type: "loop",
-          name: "lv3",
-          tachie: "lv3.gif"
-        },
-        {
-          type: "loop",
-          name: "lv4",
-          tachie: "lv4.gif"
-        },
-        {
-          type: "loop",
-          name: "lv5",
-          tachie: "lv5.gif"
-        }
-      ]
+      currentMenu: {},
+      challengeMenuDialog: false,
+      loopMenu: {
+        name: "循环挑战",
+        type: "loop",
+        menuList: [
+          {
+            name: "lv1",
+            tachie: "lv1.gif"
+          },
+          {
+            name: "lv2",
+            tachie: "lv2.gif"
+          },
+          {
+            name: "lv3",
+            tachie: "lv3.gif"
+          },
+          {
+            name: "lv4",
+            tachie: "lv4.gif"
+          },
+          {
+            name: "lv5",
+            tachie: "lv5.gif"
+          }
+        ]
+      },
+      equiMenu: {
+        name: "装备挑战",
+        type: "equi",
+        menuList: [
+          {
+            name: "lv1",
+            tachie: "equi.gif"
+          },
+          {
+            name: "lv2",
+            tachie: "equi.gif"
+          },
+          {
+            name: "lv3",
+            tachie: "equi.gif"
+          },
+          {
+            name: "lv4",
+            tachie: "equi.gif"
+          },
+          {
+            name: "lv5",
+            tachie: "equi.gif"
+          }
+        ]
+      },
+      
     }
   },
   props: ["item"],
   mounted() {},
   watch: {},
-  methods: {}
+  methods: {
+    resetCurrentMonster(v){
+      console.log(v)
+    },
+    openMenu(type) {
+      if(this.currentMenu.type == type){
+        this.currentMenu = {}
+        return this.challengeMenuDialog = false
+      }
+      this.challengeMenuDialog = true
+      switch (type) {
+        case "loop":
+          this.currentMenu = this.loopMenu
+          break
+        case "equi":
+          this.currentMenu = this.equiMenu
+          break
+        case "relic":
+          this.currentMenu = this.relicMenu
+          break
+        case "boss":
+          this.currentMenu = this.bossMenu
+          break
+
+        default:
+          break
+      }
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -100,7 +156,8 @@ export default {
     right: 0;
     // background: linear-gradient(210.55deg, #ebf2f9 19.14%, #c6d7eb 154.68%);
     background: #292929;
-    box-shadow: inset -2px -2px 3px #9a9393, inset 1px 1px 1px rgba(117,117 ,117,0.5);
+    box-shadow: inset -2px -2px 3px #9a9393,
+      inset 1px 1px 1px rgba(117, 117, 117, 0.5);
     border: 1px solid #ccc;
     padding: 10px 15px;
     border-radius: 4px;
@@ -142,8 +199,8 @@ export default {
             transform: translate(-50%, -50%);
           }
         }
-        .dname{
-          margin-top:4px;
+        .dname {
+          margin-top: 4px;
         }
       }
     }
@@ -175,4 +232,3 @@ export default {
   }
 }
 </style>
-
